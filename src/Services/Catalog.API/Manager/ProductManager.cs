3a0737a4a9913c9db7a049607e3cd0eaded7e0be
Saikat Rahman <saikatrahmanofficial@gmail.com>
2023-09-07 19:50:05 +0600

    creating a seed method

---
+++ src/Services/Catalog.API/Manager/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.Interfaces.Manager;
+using Catalog.API.Models;
+using Catalog.API.Repository;
+using MongoRepo.Manager;
+using MongoRepo.Repository;
+
+namespace Catalog.API.Manager
+{
+    public class ProductManager : CommonManager<Product>, IProductManager
+    {
+        public ProductManager() : base(new ProductRepository())
+        {
+
+        }
+    }
+}
