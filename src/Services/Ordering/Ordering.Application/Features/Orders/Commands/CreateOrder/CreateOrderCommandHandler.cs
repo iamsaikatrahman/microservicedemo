92abcc32ebd987f6910290edccda0635f36d204c
Saikat Rahman <saikatrahmanofficial@gmail.com>
2023-10-06 23:36:46 +0600

    order controller

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
+using Ordering.Application.Contacts.Infrastructure;
 using Ordering.Application.Contacts.Persistence;
+using Ordering.Application.Models;
 using Ordering.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -13,20 +15,32 @@ namespace Ordering.Application.Features.Orders.Commands.CreateOrder
     public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, bool>
     {
         IOrderRepository _orderRepository;
+        IEmailService _emailService;
         IMapper _mapper;
 
         public CreateOrderCommandHandler(
-            IOrderRepository orderRepository, 
-            IMapper mapper)
+            IOrderRepository orderRepository,
+            IMapper mapper,
+            IEmailService emailService)
         {
             _orderRepository = orderRepository;
             _mapper = mapper;
+            _emailService = emailService;
         }
 
         public async Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
             var order = _mapper.Map<Order>(request);
-            return await _orderRepository.AddAsync(order);
+            bool isOrderPlaced = await _orderRepository.AddAsync(order);
+            if (isOrderPlaced) 
+            {
+                Email email = new Email();
+                email.To = order.UserName;
+                email.Subject = "Your Order has been placed.";
+                email.Body = $"Dear {order.FirstName + " " + order.LastName}  <br/><br/> We are excited for you to received your order #{order.Id} and with notify you one it's way. <br/> Thank you for ordering form SK Corporation.";
+                await _emailService.SendEmailAsync(email);
+            }
+            return isOrderPlaced;
         }
     }
 }
