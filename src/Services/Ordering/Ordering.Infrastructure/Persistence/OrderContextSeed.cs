27d59530ad513d89b51aa80501695085417691c5
Saikat Rahman <saikatrahmanofficial@gmail.com>
2023-10-08 06:48:40 +0600

    seed data

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().HasData(
+                new Order() 
+                {
+                    Id = 1,
+                    UserName = "saikat@gmail.com",
+                    FirstName = "Saikat",
+                    LastName = "Rahman",
+                    EmailAddress = "saikat@gmail.com",
+                    Address = "Dhaka",
+                    TotalPrice = 100,
+                    City = "Dhaka"
+                }
+                );
+        }
+    }
+}
